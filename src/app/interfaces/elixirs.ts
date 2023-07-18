6d9879bf3989acec9118af8bb441968d3a914446
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-07-17 22:39:41 -0300

    interfaces

---
+++ src/app/interfaces/elixirs.ts
@@ -0,0 +1,221 @@
+import { Ingredients } from "./ingredients";
+
+export interface Elixirs {
+  id:              string;
+  name:            string;
+  effect:          null | string;
+  sideEffects:     null | string;
+  characteristics: null | string;
+  time:            null | string;
+  difficulty:      Difficulty;
+  ingredients:     Ingredients[];
+  inventors:       Inventor[];
+  manufacturer:    null | string;
+}
+
+export enum Difficulty {
+  Advanced = "Advanced",
+  Beginner = "Beginner",
+  Moderate = "Moderate",
+  OneOfAKind = "OneOfAKind",
+  OrdinaryWizardingLevel = "OrdinaryWizardingLevel",
+  Unknown = "Unknown",
+}
+
+export interface Inventor {
+  id:        string;
+  firstName: null | string;
+  lastName:  string;
+}
+
+export class Convert {
+  public static toElixirs(json: string): Elixirs[] {
+      return cast(JSON.parse(json), a(r("Elixirs")));
+  }
+
+  public static elixirsToJson(value: Elixirs[]): string {
+      return JSON.stringify(uncast(value, a(r("Elixirs"))), null, 2);
+  }
+}
+
+function invalidValue(typ: any, val: any, key: any, parent: any = ''): never {
+  const prettyTyp = prettyTypeName(typ);
+  const parentText = parent ? ` on ${parent}` : '';
+  const keyText = key ? ` for key "${key}"` : '';
+  throw Error(`Invalid value${keyText}${parentText}. Expected ${prettyTyp} but got ${JSON.stringify(val)}`);
+}
+
+function prettyTypeName(typ: any): string {
+  if (Array.isArray(typ)) {
+      if (typ.length === 2 && typ[0] === undefined) {
+          return `an optional ${prettyTypeName(typ[1])}`;
+      } else {
+          return `one of [${typ.map(a => { return prettyTypeName(a); }).join(", ")}]`;
+      }
+  } else if (typeof typ === "object" && typ.literal !== undefined) {
+      return typ.literal;
+  } else {
+      return typeof typ;
+  }
+}
+
+function jsonToJSProps(typ: any): any {
+  if (typ.jsonToJS === undefined) {
+      const map: any = {};
+      typ.props.forEach((p: any) => map[p.json] = { key: p.js, typ: p.typ });
+      typ.jsonToJS = map;
+  }
+  return typ.jsonToJS;
+}
+
+function jsToJSONProps(typ: any): any {
+  if (typ.jsToJSON === undefined) {
+      const map: any = {};
+      typ.props.forEach((p: any) => map[p.js] = { key: p.json, typ: p.typ });
+      typ.jsToJSON = map;
+  }
+  return typ.jsToJSON;
+}
+
+function transform(val: any, typ: any, getProps: any, key: any = '', parent: any = ''): any {
+  function transformPrimitive(typ: string, val: any): any {
+      if (typeof typ === typeof val) return val;
+      return invalidValue(typ, val, key, parent);
+  }
+
+  function transformUnion(typs: any[], val: any): any {
+      const l = typs.length;
+      for (let i = 0; i < l; i++) {
+          const typ = typs[i];
+          try {
+              return transform(val, typ, getProps);
+          } catch (_) {}
+      }
+      return invalidValue(typs, val, key, parent);
+  }
+
+  function transformEnum(cases: string[], val: any): any {
+      if (cases.indexOf(val) !== -1) return val;
+      return invalidValue(cases.map(a => { return l(a); }), val, key, parent);
+  }
+
+  function transformArray(typ: any, val: any): any {
+      if (!Array.isArray(val)) return invalidValue(l("array"), val, key, parent);
+      return val.map(el => transform(el, typ, getProps));
+  }
+
+  function transformDate(val: any): any {
+      if (val === null) {
+          return null;
+      }
+      const d = new Date(val);
+      if (isNaN(d.valueOf())) {
+          return invalidValue(l("Date"), val, key, parent);
+      }
+      return d;
+  }
+
+  function transformObject(props: { [k: string]: any }, additional: any, val: any): any {
+      if (val === null || typeof val !== "object" || Array.isArray(val)) {
+          return invalidValue(l(ref || "object"), val, key, parent);
+      }
+      const result: any = {};
+      Object.getOwnPropertyNames(props).forEach(key => {
+          const prop = props[key];
+          const v = Object.prototype.hasOwnProperty.call(val, key) ? val[key] : undefined;
+          result[prop.key] = transform(v, prop.typ, getProps, key, ref);
+      });
+      Object.getOwnPropertyNames(val).forEach(key => {
+          if (!Object.prototype.hasOwnProperty.call(props, key)) {
+              result[key] = transform(val[key], additional, getProps, key, ref);
+          }
+      });
+      return result;
+  }
+
+  if (typ === "any") return val;
+  if (typ === null) {
+      if (val === null) return val;
+      return invalidValue(typ, val, key, parent);
+  }
+  if (typ === false) return invalidValue(typ, val, key, parent);
+  let ref: any = undefined;
+  while (typeof typ === "object" && typ.ref !== undefined) {
+      ref = typ.ref;
+      typ = typeMap[typ.ref];
+  }
+  if (Array.isArray(typ)) return transformEnum(typ, val);
+  if (typeof typ === "object") {
+      return typ.hasOwnProperty("unionMembers") ? transformUnion(typ.unionMembers, val)
+          : typ.hasOwnProperty("arrayItems")    ? transformArray(typ.arrayItems, val)
+          : typ.hasOwnProperty("props")         ? transformObject(getProps(typ), typ.additional, val)
+          : invalidValue(typ, val, key, parent);
+  }
+
+  if (typ === Date && typeof val !== "number") return transformDate(val);
+  return transformPrimitive(typ, val);
+}
+
+function cast<T>(val: any, typ: any): T {
+  return transform(val, typ, jsonToJSProps);
+}
+
+function uncast<T>(val: T, typ: any): any {
+  return transform(val, typ, jsToJSONProps);
+}
+
+function l(typ: any) {
+  return { literal: typ };
+}
+
+function a(typ: any) {
+  return { arrayItems: typ };
+}
+
+function u(...typs: any[]) {
+  return { unionMembers: typs };
+}
+
+function o(props: any[], additional: any) {
+  return { props, additional };
+}
+
+function m(additional: any) {
+  return { props: [], additional };
+}
+
+function r(name: string) {
+  return { ref: name };
+}
+
+const typeMap: any = {
+  "Elixirs": o([
+      { json: "id", js: "id", typ: "" },
+      { json: "name", js: "name", typ: "" },
+      { json: "effect", js: "effect", typ: u(null, "") },
+      { json: "sideEffects", js: "sideEffects", typ: u(null, "") },
+      { json: "characteristics", js: "characteristics", typ: u(null, "") },
+      { json: "time", js: "time", typ: u(null, "") },
+      { json: "difficulty", js: "difficulty", typ: r("Difficulty") },
+      { json: "ingredients", js: "ingredients", typ: a(r("Ingredient")) },
+      { json: "inventors", js: "inventors", typ: a(r("Inventor")) },
+      { json: "manufacturer", js: "manufacturer", typ: u(null, "") },
+  ], false),
+  "Ingredient": o([
+      { json: "id", js: "id", typ: "" },
+      { json: "name", js: "name", typ: "" },
+  ], false),
+  "Inventor": o([
+      { json: "id", js: "id", typ: "" },
+      { json: "firstName", js: "firstName", typ: u(null, "") },
+      { json: "lastName", js: "lastName", typ: "" },
+  ], false),
+  "Difficulty": [
+      "Advanced",
+      "Beginner",
+      "Moderate",
+      "OneOfAKind",
+      "OrdinaryWizardingLevel",
+      "Unknown",
+  ],
+};
